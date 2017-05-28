0a5436c0b8a075142acded844ef1f260c48d093c
Matthew Yeo <atomizewhitewhey@gmail.com>
2017-05-28 18:40:31 +0800

    Added function names

--- KaggleQuora.py
+++ KaggleQuora.py
@@ -12,9 +12,6 @@ questions1 = train['question1'].tolist()
 questions2 = train['question2'].tolist()
 
 
-
-
-
 import string
 sp = string.punctuation
 
@@ -95,6 +92,7 @@ questions2 = rmstop(questions2)
 
 from nltk.stem.porter import PorterStemmer
 porter_stemmer = PorterStemmer()
+
 def stem(questions):
     temp = [question.split() for question in questions] 
     temp = map(lambda t: [porter_stemmer.stem(w) for w in t], temp)
@@ -107,7 +105,74 @@ questions2 = stem(questions2)
 ##wf = to_TF(questions1)
 ##wf_bar(wf)
 ##plot_cfd(wf)
+questions1n = normalize(questions1)
+questions2n = normalize(questions2)
+questions1n = rmstop(questions1n)
+questions2n = rmstop(questions2n)
+questions1n = stem(questions1n)
+questions2n = stem(questions2n)
 
+def numwordf(questions1n, questions2n):
+    temp1 = [question.split() for question in questions1n] 
+    temp2 = [question.split() for question in questions2n]
+    numwords = []
+    for i in range(len(temp1)):
+        temp3 = 0
+        for z in temp1[i]:
+            if z in temp2[i]:
+                temp3 = temp3 + 1
+        numwords.append(temp3)
+    return numwords
 
-print(questions1[:10])
-print(questions2[:10])
+numwords = numwordf(questions1n, questions2n)
+
+def stopwords(questions):
+    temp = [question.split() for question in questions]
+    questions = [' '.join([word for word in question if word in set(stop_words)]) for question in temp]
+    return questions
+
+questions1stop = normalize(questions1)
+questions2stop = normalize(questions2)
+questions1stop = stopwords(questions1stop)
+questions2stop = stopwords(questions2stop)
+
+def numstopf(questions1stop, questions2stop):  
+    temp1 = [question.split() for question in questions1stop] 
+    temp2 = [question.split() for question in questions2stop]
+    numstop = []
+    for i in range(len(temp1)):
+        temp3 = 0
+        for z in temp1[i]:
+            if z in temp2[i]:
+                temp3 = temp3 + 1
+        numstop.append(temp3)
+    return numstop
+
+numstop = numstopf(questions1stop, questions2stop)
+print(numstop[:5])
+
+def firstword(questions):
+    questions = [question.split() for question in questions]
+    interog = []
+    for question in questions:
+        if len(question) > 0:
+            interog.append(question[0])
+        else:
+            interog.append('0')
+    return interog
+
+def sameqf(questions1, questions2):
+    interog1 = normalize(questions1)
+    interog2 = normalize(questions2)
+    interog1 = firstword(interog1)
+    interog2 = firstword(interog2)
+    sameq = []
+    for i in range(len(interog1)):
+        if interog1[i] == interog2[i]:
+            sameq.append(1)
+        else:
+            sameq.append(0)
+    return sameq
+
+sameq = sameqf(questions1, questions2)
+print(sameq[:5])
